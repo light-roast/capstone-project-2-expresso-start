89bdb65c17425a0f142fcc0d99ca3b904a84bc65
Daniel Echeverri <echeverri121@gmail.com>
2024-01-02 17:38:19 -0500

    Done post request for employees

--- src/api/employees.js
+++ src/api/employees.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const employeesRouter = express.Router();
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database(process.env.TEST_DATABASE || '../../database.sqlite');
+const path = require('path');
+const dbPath = path.join(__dirname, '../database.sqlite');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || dbPath);
 
+//Get all employees in DB
 employeesRouter.get('/', (req, res, next) => {
     db.all('SELECT * FROM Employee WHERE is_current_employee = 1', (err, employees) => {
         if (err) {
@@ -12,6 +15,40 @@ employeesRouter.get('/', (req, res, next) => {
           }
     })
   });
+
+
+//Custom middleware to chek body info when posting a new employee
+  const validateEmployeeFields = (req, res, next) => {
+    const employee = req.body.employee;
+    if (!employee || !employee.name || !employee.position || !employee.wage) {
+        res.status(400).send('Missing required fields in the request body.');
+    } else {
+        next();
+    }
+};
+
+//Post a new employee
+employeesRouter.post('/', validateEmployeeFields, (req, res, next) => {
+    const employee = req.body.employee;
+
+    db.run('INSERT INTO Employee (name, position, wage) VALUES ($name, $position, $wage)', {
+        $name: employee.name,
+        $position: employee.position,
+        $wage: employee.wage
+    }, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            db.get(`SELECT * FROM Employee WHERE id = ${this.lastID}`, (err, employee) => {
+                if (err) {
+                    next(err);
+                } else {
+                    res.status(201).json({ employee: employee });
+                }
+            });
+        }
+    });
+});
 
 
 module.exports = employeesRouter;
